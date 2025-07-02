0301f0c0437ba17ba6672d66894213f8df94395e
Khalidahsan007 <khalidasan2407@gmail.com>
2025-07-02 22:52:27 +0600

    my recipes card fixed with font bold

--- src/Pages/MyRecipes .jsx
+++ src/Pages/MyRecipes .jsx
@@ -103,11 +103,11 @@ const MyRecipes = () => {
                   <BiSolidLike></BiSolidLike> {recipe.likeCount}
                 </div>
               </h2>
-              <p>Ingredients: {recipe.ingredients}</p>
-              <p>Instructions: {recipe.instructions}</p>
-              <p>{recipe.cuisine}</p>
-              <p>Time: {recipe.time} mins</p>
-              <p>Category: {recipe.categories.join(", ")}</p>
+              <p><span className="font-semibold">Ingredients:</span> {recipe.ingredients}</p>
+              <p><span className="font-semibold">Instructions:</span> {recipe.instructions}</p>
+              <p className="font-semibold">{recipe.cuisine}</p>
+              <p><span className="font-semibold">Time:</span> {recipe.time} mins</p>
+              <p><span className="font-semibold">Category:</span> {recipe.categories.join(", ")}</p>
               <div className="card-actions justify-end">
                 <button
                   onClick={() => handleUpdateClick(recipe)}
